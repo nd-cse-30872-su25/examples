444107a6a79746c1968de075f82e9dab22d7f10c
Peter Bui <pbui@nd.edu>
2025-05-31 13:26:08 -0400

    Lecture 01: add Exercise 01-D

---
+++ lecture01/exercise01-D/solution.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Type Aliases
+
+Matrix = list[list[int]]
+
+# Functions
+
+def read_matrix() -> Matrix:
+    n = int(sys.stdin.readline())
+    return [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+def find_max_row(matrix : Matrix) -> int:
+    max_row, max_sum = 0, 0
+    for cur_row, cur_numbers in enumerate(matrix, 1):
+        cur_sum = sum(cur_numbers)
+        if cur_sum > max_sum:
+            max_row, max_sum = cur_row, cur_sum
+    return max_row
+
+# Main Execution
+
+def main():
+    while matrix := read_matrix():
+        max_row = find_max_row(matrix)
+        print(max_row)
+
+if __name__ == '__main__':
+    main()
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
