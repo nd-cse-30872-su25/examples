adfc7ce39b7b222a3d9049f88a4803e18e05f3ac
Peter Bui <pbui@bx612.space>
2025-06-04 12:31:02 -0400

    Exercise 01-D: add docstrings

--- lecture01/exercise01-D/solution.py
+++ lecture01/exercise01-D/solution.py
@@ -9,10 +9,12 @@ Matrix = list[list[int]]
 # Functions
 
 def read_matrix() -> Matrix:
+    ''' Returns n x n matrix read from stdin '''
     n = int(sys.stdin.readline())
     return [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 def find_max_row(matrix : Matrix) -> int:
+    ''' Returns row with maximum total sum '''
     max_row, max_sum = 0, 0
     for cur_row, cur_numbers in enumerate(matrix, 1):
         cur_sum = sum(cur_numbers)
@@ -23,6 +25,8 @@ def find_max_row(matrix : Matrix) -> int:
 # Main Execution
 
 def main():
+    ''' Reads in a series of matrices, finds maximum row of each matrix, and
+    prints out result '''
     while matrix := read_matrix():
         max_row = find_max_row(matrix)
         print(max_row)
